442add22335674900ed2d1cd9f87bad6d9f0b700
LennyKappa <lennykappakaiju@gmail.com>
2019-12-18 16:25:18 +0000

    added times table thing, will probably remove

--- Chapter-3/ch-3/src/main.rs
+++ Chapter-3/ch-3/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+
+  // Take in input
+
+  let mut input = String::new();
+
+  std::io::stdin().read_line(&mut input)
+    .expect("Failed to read input");
+
+  let input : u32 = input.trim().parse()
+    .expect("NaN");
+  
+  // Make our table
+  let mut x = 1;
+  let mut y = 1;
+  // let mut table : [String,input];
+
+  while x <= input{
+    
+    while y <= input{
+      print!("{:^5}", x*y);
+      y += 1;
+    }
+    print!("\n");
+    y = 1;
+    x += 1;
+  }
 }
