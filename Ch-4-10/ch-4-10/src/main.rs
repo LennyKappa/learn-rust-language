368cd1cef4af0f85a9e85ceb29d03fb95f9d7e89
LennyKappa <lennykappakaiju@gmail.com>
2020-01-31 11:15:18 -0500

    Modified ch4-10, changed Ch11 to test lib

--- Ch-4-10/ch-4-10/src/main.rs
+++ Ch-4-10/ch-4-10/src/main.rs
@@ -1,11 +1,59 @@
+use std::cmp::max;
+#[allow(dead_code)]
+
+fn largest_index<T : PartialOrd>(list: &[T]) -> &T{
+
+	let mut largest = &list[0];
+
+	for item in list.iter(){
+		if item > largest{
+			largest = &item;
+		}
+	}
+	largest
+}
+
+fn break_down(list: &mut [i32]) -> i32{
+	print!("List contents: ");
+
+	for item in list.iter(){
+		print!("{} ",item);
+	}
+
+	print!("\n");
+	if  list.len() == 1 {
+		list
+	}
+
+	let left = break_down(&list[..list.len()/2]);
+	let right = break_down(&list[list.len()/2..]);
+
+	let left_count = 0;
+	let right_count = 0;
+
+	let merged = Vec::new();
+	while left_count < left.len() && right_count < right.len(){
+		match left[left_count].cmp(&right[right.count]) {
+			Ordering::Less => {
+				merged.push(left[left_count]);
+				left_count++;
+			}
+			_ => {
+				merged.push(right[right_count]);
+				right_count++;
+			}
+		}
+	}
+
+	if(left_count < left.len()){
+
+	}
+
+	Vec::new()
+
+
+}
 fn main() {
-    let v = vec![1,2,3,4,5];
-    match v.get(2) {
-    	Some(x) => println!("v[2] = {}", x),
-    	None => println!("v[2] does not exist."),
-    }
-    match v.get(100) {
-    	Some(x) => println!("v[100] = {}", x),
-    	None => println!("v[100] does not exist."),
-    }
+    let thing = vec![1,85,214,467,325,62,67,334,32];
+    break_down(&thing);
 }
