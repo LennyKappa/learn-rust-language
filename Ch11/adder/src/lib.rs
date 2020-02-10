5d0da8665be5f5b10b3b11675df6ae28f3367343
LennyKappa <lennykappakaiju@gmail.com>
2020-02-10 10:17:39 -0500

    Adder added

--- Ch11/adder/src/lib.rs
+++ Ch11/adder/src/lib.rs
@@ -1,12 +1,26 @@
 #[cfg(test)]
 mod tests {
+	#[derive(Debug)]
+	struct Rectangle {
+	    width: u32,
+	    height: u32,
+	}
+
+	impl Rectangle {
+	    fn can_hold(&self, other: &Rectangle) -> bool {
+	        self.width > other.width && self.height > other.height
+	    }
+	}
+
     #[test]
     fn it_woks() {
         assert_eq!(2 + 2, 4);
     }
 
     #[test]
-    fn panic(){
-    	panic!("bruh");
+    fn rect(){
+    	let larger = Rectangle { width: 8, height: 7 };
+        let smaller = Rectangle { width: 5, height: 1 };
+    	assert!(!smaller.can_hold(&larger));
     }
 }
